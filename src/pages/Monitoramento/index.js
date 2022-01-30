c9075db9945655234e8ec9908d9d489718964053
elayne <elayne.natalia@outlook.com>
2022-01-30 02:11:08 -0300

    monitoramento

--- src/pages/Monitoramento/index.js
+++ src/pages/Monitoramento/index.js
@@ -3,6 +3,8 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,11 +15,58 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const planta =
-  'A irrigação não é apenas molhar o solo, mas sim quando calculamos a água que precisamos para uma cultura e a colocamos no solo conforme o planejado e o mais regularmente possível.' +
-  'Ela veio com o objetivo de ajudar as planta para atender às condições de umidade de solo visando à melhoria da produção agrícola, tanto em quantidade como em qualidade ou oportunidade.' +
-  'Na realidade, ela constitui um conjunto de operações (compondo em si um sistema) necessário ao atendimento das necessidades de água para as plantas, bem como eliminar seus excessos, que transcendem à relação solo, água, planta, pura e simplesmente.';
-
+const rows = [
+  {
+    id: 1,
+    Nome: 'Palmeira',
+    TipoSolo: 'Arenoso',
+    Gotejamento: true,
+    Irrigada: false,
+    Solenoide: 'S1',
+    Sensor: 'Se1',
+    Status: 'Ativo'
+  },
+  {
+    id: 2,
+    Nome: 'Girassol',
+    TipoSolo: 'Arenoso-Argiloso',
+    Gotejamento: true,
+    Irrigada: false,
+    Solenoide: 'S2',
+    Sensor: 'Se2',
+    Status: 'Ativo'
+  },
+  {
+    id: 3,
+    Nome: 'Cacto',
+    TipoSolo: 'Argiloso',
+    Gotejamento: true,
+    Irrigada: false,
+    Solenoide: 'S3',
+    Sensor: 'Se3',
+    Status: 'Inativo'
+  },
+  {
+    id: 4,
+    Nome: 'Cacto',
+    TipoSolo: 'Argiloso',
+    Gotejamento: true,
+    Irrigada: false,
+    Solenoide: 'S3',
+    Sensor: 'Se3',
+    Status: 'Inativo'
+  },
+  {
+    id: 5,
+    Nome: 'Cacto',
+    TipoSolo: 'Argiloso',
+    Gotejamento: true,
+    Irrigada: false,
+    Solenoide: 'S3',
+    Sensor: 'Se3',
+    Status: 'Inativo'
+  },
+];
 const title = 'Monitoramento'
 
 export default function Monitoramento() {
@@ -25,11 +74,20 @@ export default function Monitoramento() {
 
   return (
     <div>
-      <Card className={classes.root} variant="outlined">
+      <Card className={classes.root} variant="outlined" style={{ height: 480, width: '100%' }} >
         <CardContent>
           <Typography variant="h4" component="h2" color="primary">{title}</Typography>
-          <Typography variant="h7" paragraph>{planta}</Typography>
         </CardContent>
+
+        <div style={{ height: 370, marginLeft: "10px", marginRight: "10px"}}>
+          <DataGrid
+            columns={[{ field: 'Nome', minWidth: 150, type: 'string' }, { field: 'TipoSolo', minWidth: 150, type: 'string' }, 
+                    { field: 'Gotejamento', type: 'boolean', width: 120 }, { field: 'Irrigada', type: 'boolean', width: 120 },
+                    { field: 'Solenoide', type: 'string', width: 150 }, { field: 'Sensor', type: 'string', width: 120 },
+                    { field: 'Status', type: 'string', width: 150 }]}
+            rows={rows}
+          />
+        </div>
       </Card>
     </div>
   );
